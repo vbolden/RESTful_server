7ddece14bf1dea17b2bf0eb42e28c3d86737da6b
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-14 14:37:29 -0400

    imported express.Router and searchMovies function; created a search route to handle movie searches

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const axios = require("axios");
 const movieRouter = express.Router();
+const searchMovies = require("../controllers/movieController.js")
 
 // ROUTES
+movieRouter.get("/api/movies/search", searchMovies);
+
+// movieRouter.get("/api/movies/:id");
 
 module.exports = movieRouter;
