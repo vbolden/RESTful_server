0416c293787297bcbdd8438be60caf326a4ec9db
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-14 14:48:42 -0400

    imported getMovieDetails function and added it to id movieRouter

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const axios = require("axios");
 const movieRouter = express.Router();
-const searchMovies = require("../controllers/movieController.js")
+const { searchMovies, getMovieDetails } = require("../controllers/movieController.js");
 
 // ROUTES
 movieRouter.get("/api/movies/search", searchMovies);
 
-// movieRouter.get("/api/movies/:id");
+movieRouter.get("/api/movies/:id", getMovieDetails);
 
 module.exports = movieRouter;
