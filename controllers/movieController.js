454fb81f04ddbd079aa7eefbdc2e6698d7bdec33
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-14 14:25:26 -0400

    created a get request using the baseURL and query parameters for search term and apiKey

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
+require('dotenv').config();
+const apiKey = process.env.OMDB_API_KEY;
 
 const searchMovies = async (req, res) => {
     try {
         const title = req.query.title;
 
-        if(!title) {
-            return res.status(400).json({"error": "Title query parameter is required"})
+        if (!title) {
+            return res.status(400).json({ "Error": "Title query parameter is required" });
         }
-        
+
+        const apiResponse = await axios.get("http://www.omdbapi.com/", {
+            params: {
+                s: title,
+                apiKey: apiKey,
+            }
+        });
+
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while searching for movies.' });
     }
-}
+};
