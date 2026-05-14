54fad1cff9b03f1757cf9bb40ad7114170f70c96
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-14 11:53:36 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// DEPENDENCIES
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT; 
+
+// ROUTES
+// TEST ROUTE
+app.get("/", (req, res) => {
+    res.send("Testing...")
+})
+
+// PORT
+app.listen(PORT, () => {
+    console.log(`Running on PORT: ${PORT}`);
+})
