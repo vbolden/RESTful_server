b70fdb2e1a585705d8c3fd86571a927a2c6886c6
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-14 13:15:08 -0400

    imported apiKey from env file

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
+const apiKey = process.env.OMDB_API_KEY;
 
 // ROUTES
 // TEST ROUTE
